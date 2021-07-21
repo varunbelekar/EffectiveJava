ed61bac013a90d4d7f2c801e5be43ef651e56697
varun <varun.belekar@somaiya.edu>
2021-07-21 11:21:30 +0530

    Checking memory usage

--- src/main/java/com/varun/creatingobjects/unnecessaryobjs/AvoidCreatingUnnecessaryObjs.java
+++ src/main/java/com/varun/creatingobjects/unnecessaryobjs/AvoidCreatingUnnecessaryObjs.java
@@ -4,12 +4,15 @@ import java.util.concurrent.ExecutionException;
 
 public class AvoidCreatingUnnecessaryObjs {
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		long beforeUsedMem = Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
 		long startTime = System.currentTimeMillis();
-		long sum = 0;
+		Long sum = 0L;
 		for (long i = 0; i < Integer.MAX_VALUE; i++) {
 			sum += i;
 		}
 		long endTime = System.currentTimeMillis();
 		System.out.println(sum + " in " + (endTime - startTime) / 1000 + " seconds");
+		long afterUsedMem=Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
+		System.out.println(afterUsedMem - beforeUsedMem);
 	}
 }

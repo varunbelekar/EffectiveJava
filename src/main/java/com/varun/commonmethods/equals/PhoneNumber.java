85b646005c585777b9e8e93e357c4b92335a3232
varun <varun.belekar@somaiya.edu>
2021-07-22 11:33:33 +0530

    Cloning objects

--- src/main/java/com/varun/commonmethods/equals/PhoneNumber.java
+++ src/main/java/com/varun/commonmethods/equals/PhoneNumber.java
@@ -3,7 +3,7 @@ package com.varun.commonmethods.equals;
 import java.util.HashSet;
 import java.util.Set;
 
-public class PhoneNumber {
+public class PhoneNumber implements Cloneable{
 	private int areaCode;
 	private int prefix;
 	private int lineNumber;
@@ -47,6 +47,16 @@ public class PhoneNumber {
 	public String toString() {
 		return "PhoneNumber [areaCode=" + areaCode + ", prefix=" + prefix + ", lineNumber=" + lineNumber + "]";
 	}
+	
+	@Override
+	protected PhoneNumber clone() {
+		try {
+			return (PhoneNumber)super.clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 	public static void main(String[] args) {
 		PhoneNumber phoneNumber1 = new PhoneNumber(1, 91, 5555);
@@ -58,5 +68,10 @@ public class PhoneNumber {
 		phoneNumbers.add(phoneNumber2);
 		
 		System.out.println(phoneNumbers);
+		
+		PhoneNumber phoneNumber3 = phoneNumber1.clone();
+		phoneNumbers.add(phoneNumber3);
+		System.out.println(phoneNumber3.equals(phoneNumber1));
+		System.out.println(phoneNumbers.size());
 	}
 }
